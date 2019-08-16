d1bcf31156e7b1a31e2f63aa8e301280ff4876f4
Chris Lormor <clormor@palantir.com>
2019-08-16 16:39:03 +0100

    add some tree solutinos

---
+++ DataStructures/Trees/PostorderTraversal/Solution.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    public static void postOrder(Node root) {
+        System.out.println(visit(new StringBuilder(), root));
+    }
+
+    private static StringBuilder visit(StringBuilder result, Node node) {
+        if (node != null) {
+            visit(result, node.left);
+            visit(result, node.right);
+            result.append(node.data).append(" ");
+        }
+        return result;
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }
+}
